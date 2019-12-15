c9cb4438da2ebf02e78ef23fb6ad8bfb53295a22
yane-PC\yane <yane.kateliev@gmail.com>
2019-12-15 11:06:41 +0200

    Finish with C# versio

---
+++ DigitsRecognizer/DigitsRecognizer/CSharp/Evaluator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp
+{
+    public class Evaluator
+    {
+        public static double Correct(IEnumerable<Observation> validationSet, IClassifier classifier)
+        {
+            return validationSet
+                .Select(obs => Score(obs, classifier))
+                .Average();
+        }
+
+        public static double Score(Observation obs, IClassifier classifier)
+        {
+            if (classifier.Predict(obs.Pixels) == obs.Label)
+                return 1.0;
+            else
+                return 0.0;
+        }
+    }
+}
--- DigitsRecognizer/DigitsRecognizer/CSharp/Program.cs
+++ DigitsRecognizer/DigitsRecognizer/CSharp/Program.cs
@@ -6,10 +6,18 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
+            var distance = new ManhattanDistance();
+            var classifier = new BasicClassifier(distance);
+
             var trainingPath = @"..\..\..\Data\trainingsample.csv";
             var training = DataReader.ReadObservations(trainingPath);
+            classifier.Train(training);
 
-            Console.ReadLine();
+            var validationPath = @"..\..\..\Data\validationsample.csv";
+            var validation = DataReader.ReadObservations(validationPath);
+
+            var correct = Evaluator.Correct(validation, classifier);
+            Console.WriteLine($"Correctly classified:{correct}");
         }
     }
 }
